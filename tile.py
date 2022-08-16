c66e18bcb37be5a4b867456f9f72442870372c6e
celleron56 <8882008-celleron56@users.noreply.gitlab.com>
2022-08-16 22:32:07 +0000

    Replace tile.py

--- tile.py
+++ tile.py
@@ -1,18 +1,22 @@
 
 class chunk():
   def __init__(self,sizex,sizey ):
-      self.map = [sizex , sizey]
+      self.map = []
+      self.sizex = sizex
+      self.sizey = sizey
       for counter in range( 0 , sizex * sizey): 
         self.map.append("None")
         
   def setTile(self ,  x, y , tilename):
-      if (x > 0 and x < (self.map[0] + 1)) and ( y > 0 and y < (self.map[1] + 1) ):
-        coord = (y * self.map[0] + x ) - 9
+        y = y % self.sizey
+        x = x % self.sizex
+        coord = (y * self.sizex + x ) - 9
         self.map[coord] = tilename
   def returnTile(self, x , y):
-       if (x > 0 and x < (self.map[0] + 1)) and ( y > 0 and y < (self.map[1] + 1) ):
-        coord = (y * self.map[0] + x ) - 9
-        return self.map[coord] 
+       y = y % self.sizey
+       x = x % self.sizex
+       coord = (y * self.sizex + x ) - 9
+       return self.map[coord] 
         
 #################
 
