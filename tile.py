c0b02f6d232db4ee83004eebe4961d8b0f08330c
celleron56 <celleron56@protonmail.com>
2022-08-30 16:58:53 -0700

    0.51

--- tile.py
+++ tile.py
@@ -1,4 +1,6 @@
 abm = []
+import random
+import abmpy as abp
 class chunk():
   def __init__(self,sizex,sizey ):
       self.map = []
@@ -29,14 +31,14 @@ class chunk():
     nt = []
     for yd in  range(0 - radius , radius) :
      for xd in range(0 - radius , radius) :
-         x = x - int( radius / 2 )
-         y = y - int(radius / 2 )
+#          x = x - int( radius / 2 )
+#          y = y - int(radius / 2 )
          nt.append(self.returnTile(x + xd , y + yd))
     return nt   
         
 #################
 def ABM_register(code , chunk , block , chance):
-    abm.append[(code , chunk , block, floor(chance + 1))]
+    abm.append([code , chunk , block, chance + 1])
     
 def ABM_clear():
     abm = []
@@ -44,10 +46,10 @@ def ABM_clear():
 def ABM_run():
  if len(abm) > 0 :
    for thing in abm :
-       conlist = abm[thing]
-       for i in conlist[3] :
+       conlist = thing
+       for i in range(1 , conlist[3]) :
         blockx = random.randint(1 , conlist[1].sizex)
         blocky = random.randint(1 , conlist[1].sizey)
-        if conlist[1].returnTile(blockx , blocky) == abm[2] :
-           exec(conlist0)
+        if conlist[1].returnTile(blockx , blocky) == conlist[2] :
+           exec(conlist[0])
     
